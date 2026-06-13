e83561667be75403b315c554cf861bbc45830e70
Raghad Abu Dawabeh <youremail@example.com>
2026-06-13 17:40:09 +0300

    fix: optimize seeder route and clear caching

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Artisan;
+
 
 //  الصفحة الرئيسية
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -110,15 +110,6 @@ Route::delete('/wishlist/{wishlist}', [WishlistController::class, 'destroy'])->n
 
 
 
-Route::get('/run-seeder', function () {
-    try {
-        // تشغيل الميجريشن والسينج برمجياً
-        Artisan::call('migrate:fresh', ['--seed' => true]);
-        return 'تم بناء الجداول وضخ المنتجات بنجاح بفضل الله!';
-    } catch (\Exception $e) {
-        return 'حدث خطأ: ' . $e->getMessage();
-    }
-});
 
 
 
