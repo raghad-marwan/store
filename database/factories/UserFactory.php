6c4b93982ab8e674d94e3ffd351ccb8b93950ed8
Raghad Abu Dawabeh <youremail@example.com>
2026-06-13 17:08:51 +0300

    remove faker usage from user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,17 +24,14 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
-        $faker = \Faker\Factory::create();
-
         return [
-            'name' => $faker->name(),
-            'email' => $faker->unique()->safeEmail(),
+            'name' => 'Test User',
+            'email' => 'user_' . uniqid() . '@example.com', // توليد إيميل فريد بدون faker
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
-
     /**
      * Indicate that the model's email address should be unverified.
      */
